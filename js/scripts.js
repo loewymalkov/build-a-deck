591d23acffc69b6a84dab6c6c2d5f4742772b35a
loewy malkovich and nikhil batta <loewy+nikhil@>
2019-08-19 16:32:21 -0700

    add front end html formatting and output display in list format

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-var suits = [ "clubs", "spades", "diamonds", "hearts"]
-var ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
-
-
-suits.forEach(function(suit){
-  ranks.forEach(function(rank){
-  console.log(rank + " of " + suit)
+// var suits = [ "clubs", "spades", "diamonds", "hearts"]
+// var ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+//
+//
+// suits.forEach(function(suit){
+//   ranks.forEach(function(rank){
+//   console.log(rank + " of " + suit)
+//   });
+// });
+$(document).ready(function(){
+  var suits = [ "clubs", "spades", "diamonds", "hearts"]
+  var ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+  suits.forEach(function(suit){
+    ranks.forEach(function(rank){
+      $("#output").append("<li>" + rank + " of " + suit + "</li>")
+    });
   });
 });
